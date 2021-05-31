d9683591a89fc2ac09767adc617263c4f91610f4
Ian Brock <ibpositive@live.com>
2021-05-31 02:37:07 -0400

    Finalized Movement Script
    Player movement is done and ready to be refactored to work with game exits.

--- Assets/Scripts/MovementReiterate/PlayerMovement.cs
+++ Assets/Scripts/MovementReiterate/PlayerMovement.cs
@@ -15,10 +15,14 @@ public class PlayerMovement : MonoBehaviour
     
     private Vector2 movement;
     private SpriteRenderer spriteRenderer;
+    private bool isLeft;
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
 
     private void Awake()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        isLeft = false;
     }
 
     void Update()
@@ -26,25 +30,35 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        
+        
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        if (Input.GetAxisRaw("Horizontal") == 1 || 
+            Input.GetAxisRaw("Horizontal") == -1 || 
+            Input.GetAxisRaw("Vertical") == 1 || 
+            Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("LastMoveH", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastMoveY",Input.GetAxisRaw("Vertical"));
+            // inverse sprite if last position was facing left.
+            if (Input.GetAxisRaw("Horizontal") == -1)
+            {
+                isLeft = true;
+            }
+            else
+            {
+                isLeft = false;
+            }
+        }
+        
         if (Input.GetKeyDown(KeyCode.Space) && movement.sqrMagnitude == 0)
         {
             animator.SetTrigger("Attack");
         }
-        
-        if (movement.x < 0)
-        {
-            spriteRenderer.flipX = true;
-            //transform.localScale.x = transform.localScale.x * -1;
-        }
-        else
-        {
-            spriteRenderer.flipX = false;
-        }
+        spriteRenderer.flipX = isLeft;
     }
 
     private void FixedUpdate()
